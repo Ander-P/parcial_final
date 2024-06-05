9ab5852e5e479a30fd23bb9f03989ff1e57fd6aa
Miguel Herrera <hsmiguel2010@gmail.com>
2024-06-05 15:51:59 -0500

    test

--- app/src/main/java/com/example/parcialtercercorte/LoginActivity.java
+++ app/src/main/java/com/example/parcialtercercorte/LoginActivity.java
@@ -1,6 +1,15 @@
 package com.example.parcialtercercorte;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +19,63 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText edt_login_user, edt_login_password;
+    Button btn_login, btn_register;
+    SharedPreferences sharedPreferences;
+    public static final String SHARED_PREF_NAME = "userData";
+    public static final String KEY_USER = "user"; //clave para usuario
+    public static final String KEY_PASSWORD = "password"; //clave para password
+    private static final int mode_private = Context.MODE_PRIVATE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
+        edt_login_user =findViewById(R.id.edt_login_user);
+        edt_login_password =findViewById(R.id.edt_login_password);
+        btn_login =findViewById(R.id.btn_login);
+        btn_register =findViewById(R.id.btn_register);
+
+        sharedPreferences = getSharedPreferences( SHARED_PREF_NAME, mode_private);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //obtenemos los datos ingresados / convertimos a string
+                //eliminamos espacios al comienzo y final
+                String info_edt_user = edt_login_user.getText().toString().trim();
+                String info_edt_password = edt_login_password.getText().toString().trim();
+
+                //validamos campos vacios
+                if (info_edt_user.isEmpty() || info_edt_password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Rellena los cmapos vacios", Toast.LENGTH_SHORT).show();
+                }else{
+                    String savedUser = sharedPreferences.getString(KEY_USER, info_edt_user);
+                    String savedPassword = sharedPreferences.getString(KEY_PASSWORD, info_edt_password);
+
+                    //verificamos si hay datos en el sharedPreference
+                    //y si son iguales a los datos ingresados
+                    if (savedUser != null && savedPassword != null && info_edt_user.equals(savedUser) && info_edt_password.equals(savedPassword)){
+
+                        Toast.makeText(LoginActivity.this, "estas nitido", Toast.LENGTH_SHORT).show();
+                        /*Intent intent = new Intent(LoginActivity.this, );
+                        startActivity(intent);*/
+                    }else {
+                        //Usuario/contrasena incorrecta
+                        Toast.makeText(LoginActivity.this, "usuario o contraseña invalida", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/parcialtercercorte/SignInActivity.java
@@ -0,0 +1,94 @@
+package com.example.parcialtercercorte;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Pattern;
+
+public class SignInActivity extends AppCompatActivity {
+
+    EditText edt_signin_user, edt_signin_password, edt_signin_email, edt_signin_birthday;
+    Button btn_sign_in;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    public static final String SHARED_PREF_NAME= "userData";
+    public static final String KEY_USER = "user";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_BIRTHDAY = "birthday";
+    private static final int mode_private = Context.MODE_PRIVATE;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_in);
+
+        edt_signin_user = findViewById(R.id.edt_signin_user);
+        edt_signin_password = findViewById(R.id.edt_signin_password);
+        edt_signin_email = findViewById(R.id.edt_signin_email);
+        edt_signin_birthday = findViewById(R.id.edt_signin_birthday);
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, mode_private);
+        editor = sharedPreferences.edit();
+
+        String savedUser = sharedPreferences.getString(KEY_USER,  null);
+        String savedPassword = sharedPreferences.getString(KEY_PASSWORD,  null);
+
+
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String info_edt_user = edt_signin_user.getText().toString().trim();
+                String info_edt_password = edt_signin_password.getText().toString().trim();
+                String info_edt_email = edt_signin_email.getText().toString().trim();
+                String info_edt_birthday = edt_signin_birthday.getText().toString().trim();
+
+                if (!validarCampo(info_edt_user) || info_edt_user.equals("")){
+                    Toast.makeText(SignInActivity.this,"user no permitido", Toast.LENGTH_SHORT).show();
+                }else if (!validarCampo(info_edt_password) || info_edt_password.equals("")){
+                    Toast.makeText(SignInActivity.this,"password no permitida", Toast.LENGTH_SHORT).show();
+                }else if (!validarEmail(info_edt_email) || info_edt_email.equals("")){
+                    Toast.makeText(SignInActivity.this,"email no permitido", Toast.LENGTH_SHORT).show();
+                }else
+                    if (savedUser != null && savedPassword != null){
+                        editor.putString( KEY_USER, info_edt_user);
+                        editor.putString(KEY_PASSWORD, info_edt_password);
+                        editor.putString(KEY_EMAIL, info_edt_email);
+                        editor.putString(KEY_BIRTHDAY, info_edt_birthday);
+                        editor.commit();
+                        Toast.makeText(SignInActivity.this, "Credenciales guardadas", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(SignInActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                }
+            }
+        });
+
+    }
+    public boolean validarCampo(String campo){
+        boolean isValid = true;
+        for (int i=0; i<campo.length(); i++){
+            char c = campo.charAt(i);
+            if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))){
+                isValid = false;
+                break;
+            }
+        }
+        return isValid;
+    }
+
+    public boolean validarEmail(String email){
+        String emailPattern = "[a-zA-Z0-9._-]+@gmail\\.com";//estructura email
+        return Pattern.matches(emailPattern, email);//intenta coincidir la estructura con el email ingresado
+    }
+}
