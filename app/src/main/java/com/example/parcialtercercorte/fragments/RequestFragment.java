5cd432e6dabb088a01b9ed6a5b3a9cc0787ee5f3
Ander-P <pradoanderson1203@gmail.com>
2024-06-07 14:09:05 -0500

    feat: add spinner of requests

--- app/src/main/java/com/example/parcialtercercorte/fragments/RequestFragment.java
+++ app/src/main/java/com/example/parcialtercercorte/fragments/RequestFragment.java
@@ -1,66 +1,153 @@
 package com.example.parcialtercercorte.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.parcialtercercorte.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RequestFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RequestFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private Spinner spinnerComics;
+    private ArrayList<String> comicTitles;
+    private Button btnSolicitar;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    public static final String KEY_EMAIL = "email";
+    public static final String SHARED_PREF_NAME= "userData";
+    private static final int mode_private = Context.MODE_PRIVATE;
 
     public RequestFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RequestFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RequestFragment newInstance(String param1, String param2) {
-        RequestFragment fragment = new RequestFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        comicTitles = new ArrayList<>();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_request, container, false);
+        View view = inflater.inflate(R.layout.fragment_request, container, false);
+
+        spinnerComics = view.findViewById(R.id.spinner_comics);
+        btnSolicitar = view.findViewById(R.id.btn_solicitar);
+
+        fetchDataFromApi();
+
+        btnSolicitar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String selectedComic = (String) spinnerComics.getSelectedItem();
+                sendPostRequest(selectedComic);
+            }
+        });
+
+        return view;
+    }
+
+    private void fetchDataFromApi() {
+        String url = "https://nova-dev-420721.uc.r.appspot.com/marvel/comics";
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("API Response", "Respuesta de la API: " + response.toString());
+                        try {
+                            comicTitles.clear(); // Clear the list before adding new data
+                            for (int i = 0; i < response.length(); i++) {
+                                JSONObject comicObject = response.getJSONObject(i);
+                                String title = comicObject.getString("title");
+                                comicTitles.add(title);
+                            }
+                            // Usar el layout personalizado para el adaptador
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.spinner_item, comicTitles);
+                            adapter.setDropDownViewResource(R.layout.spinner_item);
+                            spinnerComics.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            Log.e("JSON Error", "Error procesando JSON", e);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("API Error", "Error al obtener datos de la API: " + error.getMessage());
+            }
+        });
+
+        queue.add(jsonArrayRequest);
+    }
+
+    private void sendPostRequest(String comicName) {
+        String url = "https://nova-dev-420721.uc.r.appspot.com/solicitude/";
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+
+        // Obtener email de SharedPreferences
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREF_NAME, mode_private);
+        String savedEmail = sharedPreferences.getString(KEY_EMAIL,  null);
+
+        // Crear el objeto JSON para la solicitud
+        Map<String, String> params = new HashMap<>();
+        params.put("email", savedEmail);
+        params.put("name", comicName);
+
+        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.POST, url, new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String msg = response.getString("msg");
+                            Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            Log.e("JSON Error", "Error procesando JSON", e);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("API Error", "Error en la solicitud POST: " + error.getMessage());
+                        Toast.makeText(getContext(), "Error en la solicitud", Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Accept", "*/*");
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+        };
+
+        queue.add(postRequest);
     }
 }
