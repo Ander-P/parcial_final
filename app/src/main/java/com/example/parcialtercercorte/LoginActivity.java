973b84cc881c4c6bfda1836bc3641f8e74b83706
Ander-P <pradoanderson1203@gmail.com>
2024-06-07 10:15:49 -0500

    feat: integrate service of login

--- app/src/main/java/com/example/parcialtercercorte/LoginActivity.java
+++ app/src/main/java/com/example/parcialtercercorte/LoginActivity.java
@@ -4,8 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,9 +12,16 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -26,16 +32,19 @@ public class LoginActivity extends AppCompatActivity {
     public static final String KEY_USER = "user"; //clave para usuario
     public static final String KEY_PASSWORD = "password"; //clave para password
     private static final int mode_private = Context.MODE_PRIVATE;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
-        edt_login_user =findViewById(R.id.edt_login_user);
-        edt_login_password =findViewById(R.id.edt_login_password);
-        btn_login =findViewById(R.id.btn_login);
-        btn_register =findViewById(R.id.btn_register);
+        edt_login_user = findViewById(R.id.edt_login_user);
+        edt_login_password = findViewById(R.id.edt_login_password);
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, mode_private);
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,37 +53,72 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                //obtenemos los datos ingresados / convertimos a string
-                //eliminamos espacios al comienzo y final
                 String info_edt_user = edt_login_user.getText().toString().trim();
                 String info_edt_password = edt_login_password.getText().toString().trim();
 
-                //validamos campos vacios
-                if (info_edt_user.isEmpty() || info_edt_password.isEmpty()){
-                    Toast.makeText(LoginActivity.this, "Rellena los cmapos vacios", Toast.LENGTH_SHORT).show();
-                }else{
-                    sharedPreferences = getSharedPreferences( SHARED_PREF_NAME, mode_private);
-
-                    String savedUser = sharedPreferences.getString(KEY_USER, "");
-                    String savedPassword = sharedPreferences.getString(KEY_PASSWORD, "");
-
-                    //verificamos si hay datos en el sharedPreference
-                    //y si son iguales a los datos ingresados
-                    if (savedUser != null && savedPassword != null && info_edt_user.equals(savedUser) && info_edt_password.equals(savedPassword)){
-                        Toast.makeText(LoginActivity.this, "estas nitido", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(LoginActivity.this, inicio.class);
-                        startActivity(intent);
-                    }else {
-                        //Usuario/contrasena incorrecta
-                        Toast.makeText(LoginActivity.this, "usuario o contraseña invalida", Toast.LENGTH_SHORT).show();
-                    }
-
+                if (info_edt_user.isEmpty() || info_edt_password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Rellena los campos vacíos", Toast.LENGTH_SHORT).show();
+                } else {
+                    loginUser(info_edt_user, info_edt_password);
                 }
             }
         });
+    }
+
+    private void loginUser(String email, String password) {
+        String url = "https://nova-dev-420721.uc.r.appspot.com/auth/login";
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JSONObject requestBody = new JSONObject();
+        try {
+            requestBody.put("email", email);
+            requestBody.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, requestBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("LoginActivity", "Respuesta de la API: " + response.toString());
+                        try {
+                            String name = response.getString("name");
+                            String email = response.getString("email");
+                            String userName = response.getString("userName");
+                            String password = response.getString("password");
+                            String birthday = response.getString("birthday");
+
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("name", name);
+                            editor.putString("email", email);
+                            editor.putString("userName", userName);
+                            editor.putString("password", password);
+                            editor.putString("birthday", birthday);
+                            editor.apply();
+
+                            Toast.makeText(LoginActivity.this, "Login exitoso", Toast.LENGTH_SHORT).show();
+                            Log.d("LoginActivity", "Login exitoso");
+                            Intent intent = new Intent(LoginActivity.this, inicio.class);
+                            startActivity(intent);
+                            finish();
+                        } catch (JSONException e) {
+                            Log.e("LoginActivity", "Error en la respuesta", e);
+                            Toast.makeText(LoginActivity.this, "Error en la respuesta", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("LoginActivity", "Error en el login: " + error.getMessage());
+                Toast.makeText(LoginActivity.this, "Error en el login: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        queue.add(jsonObjectRequest);
     }
 }
